4ef5157171c5f865b19cba073b01c2fb0ae5f8cf
Tanzim Mokammel <mtanzim@gmail.com>
2023-10-06 10:22:32 -0400

    add a script to curl

--- README.md
+++ README.md
@@ -2,4 +2,28 @@
 
 - Simple Todo list CRUD app to experiment with `rust` backend development
 - Uses [actix](https://actix.rs/) and [sqlx](https://github.com/launchbadge/sqlx)
-- To start the app, run: `DATABASE_URL=sqlite:todos.db cargo watch -x run`
+
+## Running the app
+
+To setup the database:
+
+```bash
+cargo install sqlx-cli # if not installed
+sqlx database drop
+sqlx database create
+sqlx migrate run
+```
+
+- To start the app, run:
+
+```bash
+DATABASE_URL=sqlite:todos.db cargo watch -x run
+```
+
+- To run a test script on the running server using `curl` and `jq`, run the following. Note that it will reset the database!
+
+```bash
+sqlx database reset
+chmod +x try.sh
+./try.sh
+```
---
+++ try.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+curl --location '127.0.0.1:8080/api/add' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "groceries"
+}'
+
+echo ""
+
+curl --location '127.0.0.1:8080/api/add' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "clean"
+}'
+echo ""
+
+
+curl --location '127.0.0.1:8080/api/add' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "exercise"
+}'
+echo ""
+
+curl --location '127.0.0.1:8080/api/add' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "cook"
+}'
+echo ""
+
+curl --location --request PATCH '127.0.0.1:8080/api/done/1'
+echo ""
+
+curl --location --request PATCH '127.0.0.1:8080/api/done/3'
+echo ""
+
+curl --location --request DELETE '127.0.0.1:8080/api/destroy/2'
+echo ""
+
+
+curl --location '127.0.0.1:8080/api/list' | jq
+echo ""
